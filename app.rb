ad90ce384fa7fb6b91faafffb79cadd7cedbdd22
Evan_Ratzlaff <ratzlaff92@gmail.com>
2019-08-05 12:39:46 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions unless test?
+        set :session_secret, "thislab"
+      end
+      
+      get '/' do
+      erb :index
+    end
+
+    post '/checkout' do
+        session[:item] = params[:item]
+        @session = session
+        erb :checkout
+    end
+
 end
